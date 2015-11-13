9872691b5460ec9b1fa6213cb47dbb748e11f3b9
Peter Larsen <plarsen@redhat.com>
2015-11-13 00:48:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
  <head>
-  <title>Hello OpenShift YAHOO</title>
+  <title>Hello OpenShift</title>
  </head>
  <body>
- <h1>Hello OpenShift</h1>
+ <h1>Hello OpenShift -YAHOO!</h1>
  <i>
  <?php echo "Served by: {$_SERVER['SERVER_ADDR']} at " , date("D M j G:i:s T Y"); ?>
  </i>

f53cb5dc0d4b19bd4bbf2cf28611bc053bd6421b
Peter Larsen <plarsen@redhat.com>
2016-10-04 14:53:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>Hello DUKE OpenShift</title>
  </head>
  <body>
- <h1>Hello OpenShift DEMO</h1>
+ <h1>Hello OpenShift DUKE DEMO DEMO MODE</h1>
  <i>
  <?php echo "Served by: {$_SERVER['SERVER_ADDR']} at " , date("D M j G:i:s T Y"); ?>
  </i>
